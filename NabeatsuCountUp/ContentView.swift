ce66d1705db33dc24e5929912576a0ad1708eb2a
Sakamoto Kousuke <ginkin35@gmail.com>
2020-11-07 09:24:11 +0900

    Initial Commit

---
+++ NabeatsuCountUp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NabeatsuCountUp
+//
+//  Created by 坂本弘輔 on 2020/11/07.
+//  Copyright © 2020 坂本弘輔. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
